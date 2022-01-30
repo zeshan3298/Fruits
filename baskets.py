e9c899c58f2718308d9ebb0eb143cf11b5f66bc0
zeshan3298 <zeshan3298@gmail.com>
2022-01-29 23:23:18 -0500

    updating file

--- baskets.py
+++ baskets.py
@@ -3,5 +3,5 @@ from sys import exit
 print('58 total fruit')
 print('5 types of fruit')
 print('Orange:6 Apple:5 Pineapple:4 Grapefruit:4 Watermelon:3')
-print('1 apple 2 Orange 2 Pineapple 1 Grapefruit 2 Watermelon')
+print('1 apple 2 Orange 2 Pineapple 1 Grapefruit 2 Watermelon have bee in the are over 3 days old')
 print('
