f4895f44472937432befe33191cdd487635d0c34
zeshan3298 <zeshan3298@gmail.com>
2022-01-30 10:59:32 -0500

    update

--- baskets.py
+++ baskets.py
@@ -4,4 +4,3 @@ print('58 total fruit')
 print('5 types of fruit')
 print('Number of Fruits Orange:6 Apple:5 Pineapple:4 Grapefruit:4 Watermelon:3')
 print('1 apple 2 Orange 2 Pineapple 1 Grapefruit 2 Watermelon have been in the basket for over 3 days')
-print('
