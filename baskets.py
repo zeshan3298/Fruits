e70da37316ddd29838e7d6926bcd041392941a47
zeshan3298 <zeshan3298@gmail.com>
2022-01-29 23:19:12 -0500

    new file

---
+++ baskets.py
@@ -0,0 +1,7 @@
+from sys import exit
+
+print('58 total fruit')
+print('5 types of fruit')
+print('Orange:6 Apple:5 Pineapple:4 Grapefruit:4 Watermelon:3')
+print('1 apple 2 Orange 2 Pineapple 1 Grapefruit 2 Watermelon')
+print('
